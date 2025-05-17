f1624be9bddad0c3e027e2b3ef38dbeab23619ba
Neto test <isukfedor6@gmail.com>
2025-05-17 17:07:21 +0300

    Databases-2 homewok

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
 
 
+class ScopeInlineFormSet(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        main_count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+                main_count += 1
+            if main_count > 1:
+                raise ValidationError('У статьи может быть только один основной раздел')
+        if main_count == 0:
+            raise ValidationError('Не указан основной раздел статьи')
+        return super().clean()
+    
+    
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormSet
+    extra = 1
+    
+        
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
--- 2.2-databases-2/orm_migrations/school/admin.py
+++ 2.2-databases-2/orm_migrations/school/admin.py
@@ -3,11 +3,16 @@ from django.contrib import admin
 from .models import Student, Teacher
 
 
+class StudentTeachersInline(admin.TabularInline):
+    model = Student.teachers.through
+
+
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    pass
+    inlines = [StudentTeachersInline]
+    exclude = ('teachers',)
 
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    pass
+    inlines = [StudentTeachersInline]
